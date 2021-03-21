7afbf4a93863d0bfb796b79d0c08da3ad6c53fd6
legit <muthakabrian@gmail.com>
2021-03-21 12:18:35 +0300

    add onclicklistener and toast

--- app/src/main/java/com/moringaschool/allrecipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/MainActivity.java
@@ -3,12 +3,26 @@ package com.moringaschool.allrecipe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRecipeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRecipeButton =(Button)findViewById(R.id.findRecipeButton);
+        mFindRecipeButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"Hello world!",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
