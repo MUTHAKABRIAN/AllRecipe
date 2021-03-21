a2885e6b3216dd4c57268d381368bdb3dbc96c08
legit <muthakabrian@gmail.com>
2021-03-21 15:16:58 +0300

    gather data from intent

--- app/src/main/java/com/moringaschool/allrecipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/MainActivity.java
@@ -4,26 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG =MainActivity.class.getSimpleName();
     private Button mFindRecipeButton;
+    private EditText mRecipeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRecipeEditText=(EditText)findViewById(R.id.RecipeEditText);
         mFindRecipeButton =(Button)findViewById(R.id.findRecipeButton);
         mFindRecipeButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v){
+                String recipe =mRecipeEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this,RecipeActivity.class);
+                intent.putExtra("recipe",recipe);
+//                Log.d(TAG,recipe);
+
                 startActivity(intent);
-//                Toast.makeText(MainActivity.this,"Hello world!",Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this,recipe,Toast.LENGTH_LONG).show();
 
             }
         });
