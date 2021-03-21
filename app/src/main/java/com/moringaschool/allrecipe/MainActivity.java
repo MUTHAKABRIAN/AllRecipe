0c03c146909c7ddb5ce76ee475dbbe1a36b412ae
legit <muthakabrian@gmail.com>
2021-03-21 12:25:46 +0300

    add intent that takes to recipe activity

--- app/src/main/java/com/moringaschool/allrecipe/MainActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/MainActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.allrecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this,"Hello world!",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this,RecipeActivity.class);
+                startActivity(intent);
+//                Toast.makeText(MainActivity.this,"Hello world!",Toast.LENGTH_LONG).show();
 
             }
         });
