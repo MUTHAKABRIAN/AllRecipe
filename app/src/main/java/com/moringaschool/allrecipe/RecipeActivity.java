42dd9022c14fbf2ac79430aee5b0bbe0a6725818
legit <muthakabrian@gmail.com>
2021-03-21 15:19:26 +0300

    gather data from intent

--- app/src/main/java/com/moringaschool/allrecipe/RecipeActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/RecipeActivity.java
@@ -2,13 +2,21 @@ package com.moringaschool.allrecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
+    private TextView mRecipeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        mRecipeTextView =(TextView)findViewById((R.id.recipeTextView));
+        Intent intent =getIntent();
+        String recipe =intent.getStringExtra("recipe");
+        mRecipeTextView.setText("Here are all the Recipes for: "+recipe);
+
     }
 }
