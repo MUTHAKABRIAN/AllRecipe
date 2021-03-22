daac8c198adcb0edba1243f4a18f1f325ad75323
legit <muthakabrian@gmail.com>
2021-03-22 14:27:04 +0300

    create custo array Adapter

--- app/src/main/java/com/moringaschool/allrecipe/RecipeArrayAdapter.java
+++ app/src/main/java/com/moringaschool/allrecipe/RecipeArrayAdapter.java
@@ -1,4 +1,39 @@
 package com.moringaschool.allrecipe;
 
-public class RecipeArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+
+public class RecipeArrayAdapter  extends ArrayAdapter {
+    private Context mContext;
+    private String [] mRecipes;
+    private String[] mDescriptions;
+
+//    public RecipeArrayAdapter(Context context, int resource, Context mContext, String[] mRecipes, String[] mDescriptions) {
+//        super(context, resource);
+//        this.mContext = mContext;
+//        this.mRecipes = mRecipes;
+//        this.mDescriptions = mDescriptions;
+//    }
+
+    public RecipeArrayAdapter(Context context, int resource, String[] mRecipes, String[] mDescriptions) {
+        super(context, resource);
+        this.mContext = mContext;
+        this.mRecipes = mRecipes;
+        this.mDescriptions = mDescriptions;
+    }
+
+
+    @Override
+    public Object getItem(int position){
+        String recipe =mRecipes[position];
+        String description =mDescriptions[position];
+        return String.format("%s \n short description:%s",recipe,description);
+    }
+
+    @Override
+    public int getCount(){
+        return mRecipes.length;
+    }
 }
