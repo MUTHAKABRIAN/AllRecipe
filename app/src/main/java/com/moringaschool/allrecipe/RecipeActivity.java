d31ab1afc7756a6dd2158940bef55098dbdc6138
legit <muthakabrian@gmail.com>
2021-03-22 14:26:04 +0300

    modify recipe activity

--- app/src/main/java/com/moringaschool/allrecipe/RecipeActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/RecipeActivity.java
@@ -20,7 +20,7 @@ public class RecipeActivity extends AppCompatActivity {
     @BindView(R.id.recipeTextView) TextView  mRecipeTextView;
     @BindView(R.id.listView) ListView mListView;
     private String[] recipes=new String[]{"Chicken & Chorizo jambalaya","Old Delhi-style butter chicken","Chicken madras","Creamy chicken stew","Chicken supreme","Summer chicken stew","Bang bang chicken & vegetable noodles","Poached chicken breast","Next Level fried Chicken","Easy Teriyaki chicken","Mauritian chicken curry","Lighter chicken tacos","Spring roast chicken","Cashew chicken stir-fry","Nutty chicken satay strips","Easy Roast chicken","Beer can chicken" };
-
+    private String[] descriptions =new String[]{" Spanish chicken, sweet peppers and tomatoes","Creamy chicken.","It makes an easy midweek meal for two"," Indian dish"," spices and tender pieces of chicken","summer version tender chicken thighs","vibrant spring noodle dish","lightly seasoned with salads","boneless thigh chicken","sticky Asian chicken ","a spiced tomato side dish "," Mexican dish","juicy meat and crispy chicken"," protein-packed dish ","cucumber chicken","easy roast chicken","meat really tender and juicy"};
 
 
     @Override
@@ -33,8 +33,12 @@ public class RecipeActivity extends AppCompatActivity {
 //        mListView =(ListView) findViewById(R.id.listView);
 
 
-        ArrayAdapter adapter =new ArrayAdapter(this,android.R.layout.simple_list_item_1,recipes);
+        RecipeArrayAdapter adapter = new RecipeArrayAdapter(this, android.R.layout.simple_list_item_1, recipes, descriptions);
         mListView.setAdapter(adapter);
+
+        Intent intent =getIntent();
+        String recipe =intent.getStringExtra("recipe");
+        mRecipeTextView.setText("Here are all the Recipes for: "+recipe);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -44,9 +48,7 @@ public class RecipeActivity extends AppCompatActivity {
             }
         });
 
-        Intent intent =getIntent();
-        String recipe =intent.getStringExtra("recipe");
-        mRecipeTextView.setText("Here are all the Recipes for: "+recipe);
+
 
     }
 }
