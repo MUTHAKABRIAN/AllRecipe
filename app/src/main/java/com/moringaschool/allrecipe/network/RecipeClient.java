8cdb0f886cb5d1170d91600d4f8fff7345c67580
legit <muthakabrian@gmail.com>
2021-03-30 01:22:20 +0300

    add retrofit connection

--- app/src/main/java/com/moringaschool/allrecipe/network/RecipeClient.java
+++ app/src/main/java/com/moringaschool/allrecipe/network/RecipeClient.java
@@ -1,34 +1,31 @@
 package com.moringaschool.allrecipe.network;
 
+import com.moringaschool.allrecipe.Constants;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-import static com.moringaschool.allrecipe.Constants.BASE_URL;
+
 
 public class RecipeClient {
     private static Retrofit retrofit = null;
-
     public static RecipeApi getClient(){
         OkHttpClient.Builder okHttpClient = new OkHttpClient.Builder();
-        if (retrofit == null) {
-            HttpLoggingInterceptor httpLoggingInterceptor =new HttpLoggingInterceptor();
+        if(retrofit == null){
+            HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
             httpLoggingInterceptor.level(HttpLoggingInterceptor.Level.BODY);
-
             okHttpClient.addInterceptor(httpLoggingInterceptor);
             okHttpClient.build();
-
         }
-        retrofit= new Retrofit
+        retrofit = new Retrofit
                 .Builder()
-                .baseUrl(BASE_URL)
+                .baseUrl(Constants.EDAMAM_BASE_URL)
                 .client(okHttpClient.build())
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         return retrofit.create(RecipeApi.class);
-
     }
 
 
