463efff9dc73663059127a3ac08d722a717d5bf2
legit <muthakabrian@gmail.com>
2021-03-30 01:19:15 +0300

    add base url

--- app/src/main/java/com/moringaschool/allrecipe/Constants.java
+++ app/src/main/java/com/moringaschool/allrecipe/Constants.java
@@ -1,7 +1,7 @@
 package com.moringaschool.allrecipe;
 
 public class Constants {
-    public static final String BASE_URL= "https://api.edamam.com/";
+    public static final String EDAMAM_BASE_URL = "https://api.edamam.com/";
 
 
 
