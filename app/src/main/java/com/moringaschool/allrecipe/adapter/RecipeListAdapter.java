85e3a336e5d021d0b58680bd4f4d2e0106a2edfc
legit <muthakabrian@gmail.com>
2021-03-30 23:36:10 +0300

    add business logic to recipelist class

--- app/src/main/java/com/moringaschool/allrecipe/adapter/RecipeListAdapter.java
+++ app/src/main/java/com/moringaschool/allrecipe/adapter/RecipeListAdapter.java
@@ -1,92 +1,85 @@
-//package com.moringaschool.allrecipe.adapter;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.moringaschool.allrecipe.R;
-//import com.moringaschool.allrecipe.models.Recipe;
-//import com.moringaschool.allrecipe.ui.RecipeDetailActivity;
-//import com.squareup.picasso.Picasso;
-//
-//import org.parceler.Parcels;
-//
-//import java.util.List;
-//
-//import butterknife.BindView;
-//import butterknife.ButterKnife;
-//
-//
-//
-//public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
-//    private List<Recipe> mRecipes;
-//    private Context mContext;
-//
-//
-//    public RecipeListAdapter(List<Recipe> mRecipes, Context mContext) {
-//        this.mRecipes = mRecipes;
-//        this.mContext = mContext;
-//    }
-//
-//
-//    @Override
-//    public RecipeListAdapter.RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
-//        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
-//        return viewHolder;
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(RecipeViewHolder holder, int position) {
-//        holder.bindRecipe(mRecipes.get(position));
-//
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return mRecipes.size();
-//    }
-//
-//    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-//        @BindView(R.id.recipeImageView)
-//        ImageView mRecipeImageView;
-//        @BindView(R.id.recipeNameTextView)
-//        TextView mNameTextView;
-//        @BindView(R.id.cuisineTypeTextView) TextView mCuisineTypeTextView;
-//
-//        private Context mContext;
-//
-//        public RecipeViewHolder(View itemView){
-//            super(itemView);
-//            ButterKnife.bind(this,itemView);
-//            mContext=itemView.getContext();
-//            itemView.setOnClickListener(this);
-//
-//
-//        }
-//        public void bindRecipe(Recipe recipe){
-//            mNameTextView.setText(recipe.getLabel());
-//            mCuisineTypeTextView.setText(recipe.getCuisineType().get(0).getTitle());
-//            Picasso.get().load(recipe.getImage()).into(mRecipeImageView);
-//
-//        }
-//
-//        @Override
-//        public void onClick(View v) {
-//            //used getAdapterPosition instead of layoutPosition
-//            int itemPosition = getAdapterPosition();
-//            Intent intent = new Intent(mContext, RecipeDetailActivity.class);
-//            intent.putExtra("position", itemPosition);
-//            intent.putExtra("recipes", Parcels.wrap(mRecipes));
-//            mContext.startActivity(intent);
-//
-//        }
-//    }
-//
-//}
+package com.moringaschool.allrecipe.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.allrecipe.R;
+import com.moringaschool.allrecipe.models.Hit;
+import com.moringaschool.allrecipe.models.Recipe;
+import com.moringaschool.allrecipe.ui.RecipeDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
+    private List<Hit> adapterHits;
+    private Context mContext;
+
+
+    public RecipeListAdapter(List<Hit> mRecipes, Context mContext) {
+        this.adapterHits = mRecipes;
+        this.mContext = mContext;
+    }
+
+
+    @Override
+    public RecipeListAdapter.RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeViewHolder holder, int position) {
+        holder.bindRecipe(adapterHits.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return adapterHits.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        @BindView(R.id.recipeImageView) ImageView mRecipeImageView;
+        @BindView(R.id.recipeNameTextView) TextView mNameTextView;
+
+        private Context mContext;
+
+        public RecipeViewHolder(View itemView){
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+            mContext=itemView.getContext();
+            itemView.setOnClickListener(this);
+
+
+        }
+        public void bindRecipe(Hit recipe){
+            Picasso.get().load(recipe.getRecipe().getImage()).into(mRecipeImageView);
+            mNameTextView.setText(recipe.getRecipe().getLabel());
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            //used getAdapterPosition instead of layoutPosition
+
+
+        }
+    }
+
+}
