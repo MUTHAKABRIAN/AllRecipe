e96dc612ac25af22bf252df9f4f43c566feb4d74
legit <muthakabrian@gmail.com>
2021-03-30 23:34:51 +0300

    modify constant class

--- app/src/main/java/com/moringaschool/allrecipe/Constants.java
+++ app/src/main/java/com/moringaschool/allrecipe/Constants.java
@@ -2,6 +2,8 @@ package com.moringaschool.allrecipe;
 
 public class Constants {
     public static final String EDAMAM_BASE_URL = "https://api.edamam.com/";
+//    public static final String api_key ="28c7682c4af597714b4790cbd28ee328";
+//    public static final String api_id ="048b63e1";
 
 
 
