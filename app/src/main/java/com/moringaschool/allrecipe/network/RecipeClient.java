50526ca5073189cfaae32a649438c97db299d34a
legit <muthakabrian@gmail.com>
2021-03-30 23:37:25 +0300

    add retrofit to app

--- app/src/main/java/com/moringaschool/allrecipe/network/RecipeClient.java
+++ app/src/main/java/com/moringaschool/allrecipe/network/RecipeClient.java
@@ -2,7 +2,14 @@ package com.moringaschool.allrecipe.network;
 
 import com.moringaschool.allrecipe.Constants;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
