06b5f05bd0a4405be4394d13e4238a082fb5263b
legit <muthakabrian@gmail.com>
2021-03-31 08:09:32 +0300

    modifyrecipe DetailActivity

--- app/src/main/java/com/moringaschool/allrecipe/ui/RecipeDetailActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/ui/RecipeDetailActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 
 import com.moringaschool.allrecipe.R;
 //import com.moringaschool.allrecipe.adapter.RecipePagerAdapter;
+import com.moringaschool.allrecipe.adapter.RecipePagerAdapter;
+import com.moringaschool.allrecipe.models.Hit;
 import com.moringaschool.allrecipe.models.Recipe;
 
 import org.parceler.Parcels;
@@ -19,8 +21,8 @@ import butterknife.ButterKnife;
 
 public class RecipeDetailActivity extends AppCompatActivity {
  @BindView(R.id.viewPager) ViewPager mViewPager;
-//    private RecipePagerAdapter adapterViewPager;
-    List<Recipe> mRecipes;
+   private RecipePagerAdapter adapterViewPager;
+    List<Hit> mRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +30,11 @@ public class RecipeDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_detail);
         ButterKnife.bind(this);
 
-        mRecipes = Parcels.unwrap(getIntent().getParcelableExtra("restaurants"));
+        mRecipes = Parcels.unwrap(getIntent().getParcelableExtra("newHits"));
         int startingPosition = getIntent().getIntExtra("position", 0);
 
-//        adapterViewPager = new RecipePagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mRecipes);
-//        mViewPager.setAdapter(adapterViewPager);
-//        mViewPager.setCurrentItem(startingPosition);
+        adapterViewPager = new RecipePagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, mRecipes);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
