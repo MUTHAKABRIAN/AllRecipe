cf390e8168ddae155b038bf69a03c61e43207b72
legit <muthakabrian@gmail.com>
2021-03-31 20:24:05 +0300

    implement sharedPreference

--- app/src/main/java/com/moringaschool/allrecipe/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/ui/MainActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.moringaschool.allrecipe.Constants;
 import com.moringaschool.allrecipe.R;
 
 import butterknife.BindView;
@@ -19,7 +22,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public static final String TAG = MainActivity.class.getSimpleName();
 //    private Button mFindRecipeButton;
 //    private EditText mRecipeEditText;
-
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @SuppressLint("NonConstantResourceId")
     @BindView(R.id.findRecipeButton)
@@ -35,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
 //        mRecipeEditText=(EditText)findViewById(R.id.RecipeEditText);
 //        mFindRecipeButton =(Button)findViewById(R.id.findRecipeButton);
         mFindRecipeButton.setOnClickListener(this);
@@ -44,6 +51,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void onClick (View v){
             if (v == mFindRecipeButton) {
                 String recipe = mRecipeEditText.getText().toString();
+                if (!(recipe).equals("") ){
+                    addToSharedPreferences(recipe);
+                }
+
                 Intent intent = new Intent(MainActivity.this, RecipeListActivity.class);
                 intent.putExtra("recipe", recipe);
 //                Log.d(TAG,recipe);
@@ -52,6 +63,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                Toast.makeText(MainActivity.this,recipe,Toast.LENGTH_LONG).show();
 
             }
+        }
+        private void addToSharedPreferences(String recipe){
+        mEditor.putString(Constants.PREFERENCES_RECIPE_KEY,recipe).apply();
         }
     }
 
