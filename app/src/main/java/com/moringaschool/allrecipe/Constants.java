c38883aee46a7e4a431896b35ce3901a831bfa22
legit <muthakabrian@gmail.com>
2021-03-31 20:23:05 +0300

    add sharedPreference in constant class

--- app/src/main/java/com/moringaschool/allrecipe/Constants.java
+++ app/src/main/java/com/moringaschool/allrecipe/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
     public static final String EDAMAM_BASE_URL = "https://api.edamam.com/";
 //    public static final String api_key ="28c7682c4af597714b4790cbd28ee328";
 //    public static final String api_id ="048b63e1";
+    public static final String PREFERENCES_RECIPE_KEY ="recipe";
 
 
 
