96fa8590f2635297e7cb8b35a728069c16318197
legit <muthakabrian@gmail.com>
2021-04-02 15:38:32 +0300

    add node to save recipe

--- app/src/main/java/com/moringaschool/allrecipe/Constants.java
+++ app/src/main/java/com/moringaschool/allrecipe/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
 //    public static final String api_key ="28c7682c4af597714b4790cbd28ee328";
 //    public static final String api_id ="048b63e1";
     public static final String PREFERENCES_RECIPE_KEY ="recipe";
+    public static final String FIREBASE_CHILD_SEARCHED_RECIPE ="searchedRecipe";
+    public static final String FIREBASE_CHILD_RECIPES="recipes";
+
 
 
 
