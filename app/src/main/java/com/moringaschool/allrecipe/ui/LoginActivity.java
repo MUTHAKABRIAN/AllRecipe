13fe27bcddbf75fb15cc660283018b350b430273
legit <muthakabrian@gmail.com>
2021-04-05 13:04:59 +0300

    add logic to my login activity

--- app/src/main/java/com/moringaschool/allrecipe/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/ui/LoginActivity.java
@@ -2,12 +2,39 @@ package com.moringaschool.allrecipe.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.moringaschool.allrecipe.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+
+
+        }
+
     }
 }
