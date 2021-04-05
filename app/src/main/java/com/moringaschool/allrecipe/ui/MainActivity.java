a86a61cfd748bf44252db8a341a0a0d727dd256a
legit <muthakabrian@gmail.com>
2021-04-05 14:49:35 +0300

    add logic to logout

--- app/src/main/java/com/moringaschool/allrecipe/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/allrecipe/ui/MainActivity.java
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,7 +26,7 @@ import com.moringaschool.allrecipe.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
 //    private Button mFindRecipeButton;
 //    private EditText mRecipeEditText;
@@ -40,9 +41,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @BindView(R.id.AppNameTextView)
     TextView mAppNameTxtView;
-    @BindView(R.id.savedRecipesButton)Button mSavedRecipesButton;
+    @BindView(R.id.savedRecipesButton)
+    Button mSavedRecipesButton;
 
-private ValueEventListener mSearchedRecipeReferenceListener;
+    private ValueEventListener mSearchedRecipeReferenceListener;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
