6b4c1acad8bb8d80d78b81c36471cca4ba7b043e
legit <muthakabrian@gmail.com>
2021-04-08 11:45:06 +0300

    create class that will be called when user begin a drag and drop interaction

--- app/src/main/java/com/moringaschool/allrecipe/util/OnStartDragListener.java
+++ app/src/main/java/com/moringaschool/allrecipe/util/OnStartDragListener.java
@@ -1,4 +1,7 @@
 package com.moringaschool.allrecipe.util;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
