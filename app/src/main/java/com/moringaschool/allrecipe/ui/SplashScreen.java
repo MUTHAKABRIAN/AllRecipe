29c6befde3012088bb9b9ee27d825e479ad79def
legit <muthakabrian@gmail.com>
2021-04-11 23:23:12 +0300

    create logic for splash

--- app/src/main/java/com/moringaschool/allrecipe/ui/SplashScreen.java
+++ app/src/main/java/com/moringaschool/allrecipe/ui/SplashScreen.java
@@ -1,4 +1,29 @@
 package com.moringaschool.allrecipe.ui;
 
-public class SplashScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.moringaschool.allrecipe.R;
+import com.moringaschool.allrecipe.ui.LoginActivity;
+import com.moringaschool.allrecipe.ui.MainActivity;
+
+public class SplashScreen extends Activity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
 }
