c4032fedd5c6d829554266c237623c7e7dedd39b
legit <muthakabrian@gmail.com>
2021-04-12 00:45:05 +0300

    implement splash screen animation

--- app/src/main/java/com/moringaschool/allrecipe/ui/SplashScreen.java
+++ app/src/main/java/com/moringaschool/allrecipe/ui/SplashScreen.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.moringaschool.allrecipe.R;
 import com.moringaschool.allrecipe.ui.LoginActivity;
@@ -15,6 +19,17 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashfile);
+
+
+        TextView view =findViewById(R.id.text);
+        view.setText("Afri-Recipe");
+        Animation anim_text = AnimationUtils.loadAnimation(this,R.anim.slide_in_bottom);
+        view.startAnimation(anim_text);
+
+        ImageView imageView =findViewById(R.id.logo_id);
+        imageView.setImageResource(R.drawable.logo_spoon_clipart_5285972);
+        Animation anim_image =AnimationUtils.loadAnimation(this,R.anim.slide_in_top);
+        imageView.startAnimation(anim_image);
 
         handler=new Handler();
         handler.postDelayed(new Runnable() {
